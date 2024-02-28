7bab3844ef31e94237e108aa1044e8a456619e26
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2024-02-28 14:56:25 +0700

    add : all model every migration

---
+++ app/Models/Borrowing.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Borrowing extends Model
+{
+    use HasFactory;
+
+    protected $table = 'borrowings';
+    protected $guarded = ['id'];
+}
