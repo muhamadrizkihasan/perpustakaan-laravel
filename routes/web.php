d1c24445286e7bc07e27be65ee44b1b33c922a6e
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2024-02-28 16:15:23 +0700

    Add : Route Resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowingController;
+use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\RayonController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentGroupController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('students', StudentController::class);
+Route::resource('studentGroups', StudentGroupController::class);
+Route::resource('rayons', RayonController::class);
+Route::resource('publishers', PublisherController::class);
+Route::resource('books', BookController::class);
+Route::resource('borrowings', BorrowingController::class);
